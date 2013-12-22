341886d2efd377e310dd443ede0c6d0a9b9b7dd8
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2013-12-22 15:06:29 -0200

    Inclui link nos títulos nível 4 com id
    Signed-off-by: Rogerio Prado de Jesus <rogeriopradoj@gmail.com>

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,24 @@
-console.log('This would be the main JS file.');
+(function(){
+    // pegar todos os h4 com id
+    var headings = $( "h4[id]" );
+
+    // adicionar link em todos os h4
+    headings.each(function() {
+        console.log(this);
+
+        // pegar conteúdo atual
+        var content = this.innerHTML;
+        console.log(content);
+
+        // pegar o id
+        var id = this.id;
+        console.log(id);
+
+        // colocar uma âncora no conteúdo
+        content = '<a href="#' + id + '">' + content + '</a>';
+
+        // troca content
+        this.innerHTML = content;
+    });
+
+})();
