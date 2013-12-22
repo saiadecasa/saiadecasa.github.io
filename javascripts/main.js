a5d983fad49deaa3fc170d555d746dfc8636daf6
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2013-12-22 18:57:52 -0200

    Coloca link em todos h4 (tendo ou não id)
    Se tiver id, este é usado. Quando não tem id, é feito um slug do
    conteúdo.
    
    Signed-off-by: Rogerio Prado de Jesus <rogeriopradoj@gmail.com>

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,28 +1,29 @@
 (function(){
-    // pegar todos os h4 com id
-    var headings = $( "h4[id]" );
+    // pegar todos os h4
+    var headings = $( "h4" );
 
     // adicionar link em todos os h4
     headings.each(function() {
-        console.log(this);
 
         // pegar conteúdo atual
         var content = this.innerHTML;
         console.log(content);
 
-        // monta slug
-        var slug = getSlug(content);
-        console.log(slug);
+        if (this.id) {
+            // usar o id da tag, se houver
+            var link = this.id;
+            // remover id do h4
+            this.id = null;
+        } else {
+            // monta slug, se não houver id
+            var link = getSlug(content);
+        }
 
-        // colocar uma âncora no conteúdo
+        // colocar link no conteúdo
         content = '<a class="anchor" href="#' + slug + '" name="' + slug + '">'
             + '<span class="awesome awesome-link"></span></a>' + content;
 
-        // troca content
+        // trocar content
         this.innerHTML = content;
-
-        // remove id do h4
-        this.id = null;
     });
-
 })();
