3001bf3690c5e1355ed8b84b5d317f3db3573ba6
Giorgio Fellipe <giorgiofellipe@gmail.com>
2015-05-10 13:47:02 -0300

    Remove ventos anteriores a 10/05/15

--- javascripts/events.js
+++ javascripts/events.js
@@ -2,36 +2,12 @@
 
         var events = [
             {
-                titulo : "Alagoas Dev Day",
-                data : "18 de Abril",
-                local : "Colégio Contato",
-                endereco : "Rua Prof. Silvio de Macedo, 125, Jatiúca, Maceió - AL",
-                embed_link : "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3933.405863603271!2d-35.70357200000001!3d-9.646319!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x55baf0344682c769!2sCol%C3%A9gio+Contato!5e0!3m2!1spt-BR!2sbr!4v1423048047491",
-                link : "http://alagoasdevday.com.br/"
-            },
-            {
-                titulo : "PHP Experience 2015",
-                data : "25 de Abril de 2015",
-                local : "São Paulo . Hotel Renaissance",
-                endereco : "Alameda Santos, 2233",
-                embed_link : "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14629.077271259786!2d-46.6624!3d-23.558769!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb62609d26a3a7180!2sRenaissance+Sao+Paulo+Hotel!5e0!3m2!1sen!2sbr!4v1424810909289",
-                link : "http://phpexperience.imasters.com.br/"
-            },
-            {
                 titulo : "Python Nordeste 2015",
                 data : "14 a 16 de Maio",
                 local : "Natal - Rio Grande do Norte",
                 endereco : "Hotel Imirá Plaza - Via Costeira",
                 embed_link : "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969.1976254172037!2d-35.182604000000005!3d-5.827737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b2ffb83562ed77%3A0xea0923125a199def!2sImir%C3%A1+Plaza+Hotel+%26+Convention!5e0!3m2!1spt-BR!2sbr!4v1429061393672",
                 link : "http://2015.pythonnordeste.org/"
-            },
-            {
-                titulo : "Conferência CSS Brasil",
-                data : "9 de maio",
-                local : "Hotel Maksoud Plaza",
-                endereco : "Alameda Campinas, 150 - Jardins - São Paulo",
-                embed_link : "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.135260480343!2d-46.6509437!3d-23.563585099999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59b801642931%3A0xa48173eb179da934!2sAlameda+Campinas%2C+150+-+Jardim+Paulista%2C+S%C3%A3o+Paulo+-+SP!5e0!3m2!1spt-BR!2sbr!4v1429064817546",
-                link : "http://www.conferenciacssbrasil.com.br/"
             },
             {
                 titulo : "The Developers Conference - Florianópolis",
