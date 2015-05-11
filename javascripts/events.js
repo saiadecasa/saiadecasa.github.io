d7c326c66c60c71c2b14e995cae3f4baff5353bb
Giorgio Fellipe <giorgiofellipe@gmail.com>
2015-05-11 18:52:06 -0300

    Adiciona evento TDC 2015 São Paulo

--- javascripts/events.js
+++ javascripts/events.js
@@ -24,6 +24,14 @@
                 endereco : "Avenida Albert Einstein, 400 - Cidade Universitária - Campinas",
                 embed_link : "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7355.074395894319!2d-47.064721568999175!3d-22.819606489990303!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c6add6b9f5d3%3A0x82e3f5267d13e82c!2sAv.+Albert+Einstein%2C+400+-+Cidade+Universit%C3%A1ria%2C+Faculdade+de+Engenharia+El%C3%A9trica+e+Computa%C3%A7%C3%A3o+(FEEC)%2C+Universidade+Estadual+de+Campinas+-+Campinas+-+SP%2C+13083-852!5e0!3m2!1sen!2sbr!4v1424211246725",
                 link : "http://campinas.rupy.com.br/"
+            },
+            {
+                titulo : "The Developers Conference - São Paulo",
+                data : "21 a 25 de julho",
+                local : "Universidade Anhembi Morumbi",
+                endereco : "Rua Casa do Ator, 275 - Vila Olímpia - São Paulo - SP",
+                embed_link : "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.1174053028217!2d-46.677008!3d-23.600122!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce57536602d7f7%3A0xe8034f1be9c3a8b8!2sUniversidade+Anhembi+Morumbi!5e0!3m2!1spt-BR!2sbr!4v1431381035864",
+                link : "http://www.thedevelopersconference.com.br/"
             }
         ];
 
