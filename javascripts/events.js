51b62f94f3dbbf31b87b7b0ab4c5fd8b35123069
Kemel Zaidan <kemelzaidan@gmail.com>
2015-05-12 21:44:00 -0300

    Inclui 17º Encontro Locaweb de Profissionais de Internet - Rio de Janeiro

--- javascripts/events.js
+++ javascripts/events.js
@@ -15,13 +15,25 @@
             },
             {
                 titulo : "Python Nordeste 2015",
-                data : "14 a 16 de Maio",
+                data : "14 a 16 de maio",
                 local : "Natal - Rio Grande do Norte",
                 endereco : "Hotel Imirá Plaza - Via Costeira",
                 embed_link : "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969.1976254172037!2d-35.182604000000005!3d-5.827737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b2ffb83562ed77%3A0xea0923125a199def!2sImir%C3%A1+Plaza+Hotel+%26+Convention!5e0!3m2!1spt-BR!2sbr!4v1429061393672",
                 localizacao : {
                     latitude: -5.827737,
                     longitude: -35.182604000000005
+                },
+                link : "http://2015.pythonnordeste.org/"
+            },
+            {
+                titulo : "17º Encontro Locaweb de Profissionais de Internet - Rio de Janeiro",
+                data : "19 de maio",
+                local : "Hotel Sofitel Copacabana",
+                endereco : "Avenida Atlântica, 4240 - Copacabana, Rio de Janeiro - RJ, 22070-002",
+                embed_link : "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14691.911833101749!2d-43.187025!3d-22.987838!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5dc90a0eb5b7896a!2sSofitel+Rio+de+Janeiro!5e0!3m2!1spt-BR!2sbr!4v1424887670235",
+                localizacao : {
+                    latitude: -22.963075,
+                    longitude: -43.168575
                 },
                 link : "http://2015.pythonnordeste.org/"
             },
