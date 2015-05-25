0e68e050577a28c3ff11a7e30f49837086e5a4f0
Giorgio Fellipe <giorgiofellipe@gmail.com>
2015-05-25 10:11:30 -0300

    Remove eventos anteriores a 25/05/15

--- javascripts/events.js
+++ javascripts/events.js
@@ -2,42 +2,6 @@
 
         var events = [
             {
-                titulo : "The Developers Conference - Florianópolis",
-                data : "13 a 16 de maio",
-                local : "Oceania Convention Center",
-                endereco : "Rua do Marisco, 550 - Ingleses - Florianópolis",
-                embed_link : "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3541.1684351171016!2d-48.399936!3d-27.432861!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9527420773711be1%3A0xdd51edbd5dfb6a15!2sOceania+Convention+Center!5e0!3m2!1spt-BR!2sbr!4v1428577375490",
-                localizacao : {
-                    latitude: -27.432861,
-                    longitude: -48.399936
-                },
-                link : "http://www.thedevelopersconference.com.br/"
-            },
-            {
-                titulo : "Python Nordeste 2015",
-                data : "14 a 16 de maio",
-                local : "Natal - Rio Grande do Norte",
-                endereco : "Hotel Imirá Plaza - Via Costeira",
-                embed_link : "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969.1976254172037!2d-35.182604000000005!3d-5.827737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b2ffb83562ed77%3A0xea0923125a199def!2sImir%C3%A1+Plaza+Hotel+%26+Convention!5e0!3m2!1spt-BR!2sbr!4v1429061393672",
-                localizacao : {
-                    latitude: -5.827737,
-                    longitude: -35.182604000000005
-                },
-                link : "http://2015.pythonnordeste.org/"
-            },
-            {
-                titulo : "17º Encontro Locaweb de Profissionais de Internet - Rio de Janeiro",
-                data : "19 de maio",
-                local : "Hotel Sofitel Copacabana",
-                endereco : "Avenida Atlântica, 4240 - Copacabana, Rio de Janeiro - RJ, 22070-002",
-                embed_link : "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14691.911833101749!2d-43.187025!3d-22.987838!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5dc90a0eb5b7896a!2sSofitel+Rio+de+Janeiro!5e0!3m2!1spt-BR!2sbr!4v1424887670235",
-                localizacao : {
-                    latitude: -22.963075,
-                    longitude: -43.168575
-                },
-                link : "http://2015.pythonnordeste.org/"
-            },
-            {
                 titulo : "17º Encontro Locaweb de Profissionais de Internet - São Paulo",
                 data : "18 de junho",
                 local : "Fecomercio-SP",
